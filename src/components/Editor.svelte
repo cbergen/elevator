b9b8737820849d600c08c43140b25babc12f77d3
Cornelius Bergen <cornelius@matchboxcreative.com>
2024-11-16 13:08:06 -0800

    smaller default user code

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -8,27 +8,14 @@
     init: function(elevators, floors) {
         let elevator = elevators[1];
         elevator.on("idle", function() {
-            console.log('on:idle', 'elevator', this.index);
             this.goToFloor(1);
             this.goToFloor(2);
             this.goToFloor(3);
             this.goToFloor(4);
             this.goToFloor(5);
-        });
-
-        elevator = elevators[0];
-        elevator.on("idle", function() {
-            console.log('on:idle', 'elevator', this.index);
-            this.goToFloor(5);
-            this.goToFloor(4);
-            this.goToFloor(3);
-            this.goToFloor(2);
-            this.goToFloor(1);
         });
     }
 }`);
-
-	$inspect({ value });
 
 	$effect(() => {
 		game.userCodeString = value;
